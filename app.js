3dd0df3cb3ee1e38c6b48417b570711aa424d46d
Martin Johansson <maxamartin1999@gmail.com>
2020-12-23 09:48:29 +0100

    Added site.html and script.js. Initial functionality for both

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-'use strict';
+var http = require('http');
+var fs = require('fs');
 
-console.log('Hello world');
+http.createServer(function (req, res) {
+    fs.readFile('site.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    })
+}).listen(8080);
---
+++ script.js
@@ -0,0 +1,6 @@
+function loadTable() {
+    document.getElementById("title").innerHTML = "Asterix: 12 stordåd";
+    document.getElementById("rating").innerHTML = "6/10";
+    document.getElementById("place").innerHTML = "#1";
+    document.getElementById("review").innerHTML = "Asterix: 12 stordåd var en film jag tyckte om när jag var liten, ";
+}
