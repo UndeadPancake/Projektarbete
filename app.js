318df9cc27db347c214f012b0d498b47530d88f0
Martin Johansson <maxamartin1999@gmail.com>
2020-12-23 17:01:12 +0100

    Updated app.js so it works now

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 http.createServer(function (req, res) {
-    fs.readFile('site.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    })
+    console.log(req.url);
+    var q = url.parse(req.url, true);
+    var imageFile = "." + q.pathname;
+    if (req.url == "/") {
+        fs.readFile('site.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+    else if (req.url == "/script.js") {
+        fs.readFile('script.js', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.write(data);
+            res.end();
+        });
+    }
+    else {
+        fs.readFile(imageFile, function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
 }).listen(8080);
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function loadTable() {
+    document.getElementById("img").src = "asterix_12_stordad.jpg"
     document.getElementById("title").innerHTML = "Asterix: 12 stordåd";
     document.getElementById("rating").innerHTML = "6/10";
     document.getElementById("place").innerHTML = "#1";
-    document.getElementById("review").innerHTML = "Asterix: 12 stordåd var en film jag tyckte om när jag var liten, ";
+    document.getElementById("review").innerHTML = "Asterix: 12 stordåd var en film jag tyckte om när jag var liten.";
 }
