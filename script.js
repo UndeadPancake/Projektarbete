d232c392aec34528fcb8b4da93e9c302186f4db6
Martin Johansson <maxamartin1999@gmail.com>
2020-12-30 16:58:03 +0100

    Started functionality for reading from a text file.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var reader = new FileReader();
+
 function loadTable() {
-    document.getElementById("img").src = "asterix_12_stordad.jpg"
+    var text = reader.readAsText("save.txt");
+    var arr = text.split("|");
+    document.getElementById("img").src = "asterix_12_stordad.jpg";
     document.getElementById("title").innerHTML = "Asterix: 12 stordåd";
     document.getElementById("rating").innerHTML = "6/10";
     document.getElementById("place").innerHTML = "#1";
