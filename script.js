9a2281ba17e712cf5e16d4f8bf73a7f7bcfaaa14
Martin Johansson <maxamartin1999@gmail.com>
2021-01-01 16:39:46 +0100

    Removed "script didn't work" from site.html, changed some things in script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var reader = new FileReader();
+var fs = require('fs');
 
 function loadTable() {
-    var text = reader.readAsText("save.txt");
+    var text = "hej|san|jag|heter|maartin";
     var arr = text.split("|");
-    document.getElementById("img").src = "asterix_12_stordad.jpg";
-    document.getElementById("title").innerHTML = "Asterix: 12 stordåd";
-    document.getElementById("rating").innerHTML = "6/10";
-    document.getElementById("place").innerHTML = "#1";
-    document.getElementById("review").innerHTML = "Asterix: 12 stordåd var en film jag tyckte om när jag var liten.";
+    document.getElementById("img").src = arr[0];
+    document.getElementById("title").innerHTML = arr[1];
+    document.getElementById("rating").innerHTML = arr[2];
+    document.getElementById("place").innerHTML = arr[3];
+    document.getElementById("review").innerHTML = arr[4];
 }
