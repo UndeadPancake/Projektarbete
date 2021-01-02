5b72388331cd4f3ac7ee6b196d5bc567eb08b613
Martin Johansson <maxamartin1999@gmail.com>
2021-01-02 17:00:12 +0100

    Added loadReview and started loadOverwrite.

--- app.js
+++ app.js
@@ -2,23 +2,62 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+
+function loadReview(res) {
+    var text = fs.readFileSync('save.txt');
+    var arr = text.toString().split('|');
+
+    res.write("<!DOCTYPE html>\n");
+    res.write("<html>\n");
+    res.write("<head>\n");
+    res.write(" <style>\n");
+    res.write("     td {\n");
+    res.write("         border: 1px solid black;\n");
+    res.write("     }\n");
+    res.write(" </style>\n");
+    res.write("</head>\n");
+    res.write("<body>\n");
+    res.write(" <h1>Movierate</h1>\n");
+    res.write(" <h2>Rate the movies you've seen</h2>\n");
+    res.write(" <table>\n");
+    res.write("     <tr>\n");
+    res.write("         <td rowspan='3' id='img'><img src='" + arr[0] + "' /></td>\n");
+    res.write("         <td colspan='2' id='title'>" + arr[1] + "</td>\n");
+    res.write("     </tr>\n");
+    res.write("     <tr>\n");
+    res.write("         <td id='rating'>" + arr[2] + "</td>\n");
+    res.write("     </tr>\n");
+    res.write("     <tr>\n");
+    res.write("         <td colspan='2' id='review'>" + arr[3] + "</td>\n");
+    res.write("     </tr>\n");
+    res.write(" </table>\n");
+    res.write("</body>\n");
+    res.write("</html>\n");
+};
+
+function loadOverwrite(res) {
+    res.write("<!DOCTYPE html>\n");
+    res.write("<html>\n");
+    res.write("<body>\n");
+    res.write("<form>\n");
+    res.write(" <label for='img'>Movie poster</label><br>\n");
+    res.write("</form>\n");
+    res.write("</body>\n");
+    res.write("</html>\n");
+};
+
+
 http.createServer(function (req, res) {
     console.log(req.url);
     var q = url.parse(req.url, true);
     var imageFile = "." + q.pathname;
     if (req.url == "/") {
-        fs.readFile('site.html', function (err, data) {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-        });
+        loadReview(res);
+        res.end();
     }
-    else if (req.url == "/script.js") {
-        fs.readFile('script.js', function (err, data) {
-            res.writeHead(200, { 'Content-Type': 'text/javascript' });
-            res.write(data);
-            res.end();
-        });
+    else if (req.url == "/overwrite") {
+        loadOverwrite(res);
+        res.end();
     }
     else {
         fs.readFile(imageFile, function (err, data) {
--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-var fs = require('fs');
-
-function loadTable() {
-    var text = "hej|san|jag|heter|maartin";
-    var arr = text.split("|");
+function loadTable(arr) {
     document.getElementById("img").src = arr[0];
     document.getElementById("title").innerHTML = arr[1];
     document.getElementById("rating").innerHTML = arr[2];
-    document.getElementById("place").innerHTML = arr[3];
-    document.getElementById("review").innerHTML = arr[4];
+    document.getElementById("review").innerHTML = arr[3];
 }
