6ba95c7f883c9ab478b03896c0723886d8226385
Martin Johansson <maxamartin1999@gmail.com>
2021-01-03 16:42:11 +0100

    Updated CSS, removed unnessesary files.

--- script.js
+++
@@ -1,6 +0,0 @@
-function loadTable(arr) {
-    document.getElementById("img").src = arr[0];
-    document.getElementById("title").innerHTML = arr[1];
-    document.getElementById("rating").innerHTML = arr[2];
-    document.getElementById("review").innerHTML = arr[3];
-}
